72b5f260d344244b3fc0e9a9592aecebf8cb18e2
wlmnn <1366534695@qq.com>
2017-10-14 14:48:52 +0800

    wedevc

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div id="header">
+    <span>
+    </span>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+    <style src="./assets/index.css"></style>
+
+
